1ab6b57a80de1ae6048ffc759e69dabe62f56796
mayaif <margareta.ifrim@outlook.com>
2023-04-03 22:44:51 +0100

    refactor

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   development: {
-    backendUrl: `${process.env.BACKEND_URL}/api/v1/dalle`,
+    backendUrl: `${process.env.VITE_BACKEND_URL}/api/v1/dalle`,
   },
   production: {
-    backendUrl: `${process.env.BACKEND_URL}/api/v1/dalle`,
+    backendUrl: `${process.env.VITE_BACKEND_URL}/api/v1/dalle`,
   },
 };
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,5 @@ const VITE_BACKEND_URL = process.env.VITE_BACKEND_URL || ''
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    define: {
-      'process.env.BACKEND_URL': JSON.stringify(import.meta.env.VITE_BACKEND_URL),
-    },
-  },
+ 
 })
